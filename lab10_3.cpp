857bcb5f2ca0149e8dcefa4f9f347d8bc1ace62c
balewpewpew <supapitchaitan.2006@gmail.com>
2025-12-23 22:54:57 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<fstream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    double num, mean, sdev;
+    while(getline(source, textline))
+    {
+        mean += atof(textline.c_str());
+        sdev += pow(atof(textline.c_str()),2);
+        num++;
+    }
+    mean = mean/num;
+    sdev = sqrt((sdev/num)-pow(mean,2));
+    cout << "Number of data = " << num << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sdev << endl;
+}
